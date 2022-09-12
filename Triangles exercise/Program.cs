d9fdd44e825c1a7dc1084a3367375d24bd24732d
Kate <nemkovich.kate@gmail.com>
2022-09-12 23:30:22 +0300

    First stage

--- Triangles exercise/Program.cs
+++ Triangles exercise/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Windows;
+using Triangles_exercise;
+
+var pointA = RequestPoint("A");
+var pointB = RequestPoint("B");
+var pointC = RequestPoint("C");
+var triangle = new Triangle(pointA, pointB, pointC);
+
+Point RequestPoint(string pointName)
+{
+    var x = RequestCoordinate("x", pointName);
+    var y = RequestCoordinate("y", pointName);
+    return new Point(x, y);
+}
+
+double RequestCoordinate(string coordinate, string pointName)
+{
+    Console.WriteLine($"Enter coordinate {coordinate} of dot {pointName}:");
+
+    while (true)
+    {
+        if (double.TryParse(Console.ReadLine(), out var result)) return result;
+
+        Console.WriteLine("Enter data in numerical format");
+    }
+}
+
+Console.WriteLine($"Length of AB is:{triangle.LengthAB.ToString()}");
+Console.WriteLine($"Length of BC is:{triangle.LengthBC.ToString()}");
+Console.WriteLine($"Length of CA is:{triangle.LengthCA.ToString()}");
--- Triangles exercise/Triangle.cs
+++ Triangles exercise/Triangle.cs
@@ -1,6 +1,46 @@
-﻿namespace Triangles_exercise;
+﻿using System.Windows;
+
+namespace Triangles_exercise;
 
 public class Triangle
 {
-    
+    public Triangle(Point pointA, Point pointB, Point pointC)
+    {
+        PointA = pointA;
+        PointB = pointB;
+        PointC = pointC;
+        LengthAB = GetSideLength(PointA, PointB);
+        LengthBC = GetSideLength(PointB, PointC);
+        LengthCA = GetSideLength(PointC, PointA);
+    }
+
+    public Point PointA { get; set; }
+    public Point PointB { get; set; }
+    public Point PointC { get; set; }
+
+    public double LengthAB { get; }
+
+    public double LengthBC { get; }
+
+    public double LengthCA { get; }
+
+    private double GetSideLength(Point point1, Point point2)
+    {
+        return Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
+    }
+
+    public bool IsEquilateral()
+    {
+        return true;
+    }
+
+    public bool IsIsosceles()
+    {
+        return true;
+    }
+
+    public bool IsRight()
+    {
+        return true;
+    }
 }
